87a20ed89ead7ec4b7ccfa71be9977a7071fdb7c
John Todd <jqt3of5@gmail.com>
2020-08-30 15:52:35 -0600

    Updates

---
+++ precache-manifest.293d52dec0d9a232a5ac58d3e2a4d113.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85aee115ede4d3c598bff6049a6af58e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a03ecdcee954f219d7b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0bf6cf3e9b8d78e96d52",
+    "url": "/static/js/2.8d307c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d307c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a03ecdcee954f219d7b",
+    "url": "/static/js/main.62427231.chunk.js"
+  },
+  {
+    "revision": "37cfb083a556fbb1811e",
+    "url": "/static/js/runtime-main.43f0b55e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
